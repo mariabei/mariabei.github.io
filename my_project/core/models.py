266bceeedac3134825455bba4aeaffb7a4306785
MariaBei <109894811+MariaBei@users.noreply.github.com>
2023-03-06 16:42:44 +0100

    first commit

---
+++ my_project/core/models.py
@@ -0,0 +1,24 @@
+from django.db import models
+from django.contrib.auth.models import User
+
+
+class Post(models.Model):
+    title = models.CharField(max_length=255)
+    text = models.TextField()
+    author = models.ForeignKey(User, on_delete=models.CASCADE)
+    created_at = models.DateTimeField(auto_now_add=True)
+    tags = models.ManyToManyField('core.Tag')
+
+    def __str__(self):
+        return self.title
+
+
+class Tag(models.Model):
+    name = models.CharField(max_length=255)
+
+    def __str__(self):
+        return self.name
+
+
+
+
